5999ed913f07ab045bed7a00fed0ec2bd79a53b0
Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
2021-04-06 22:30:21 +0530

    Splash Screen added

---
+++ app/src/main/java/com/appchef/dishapp/SplashScreen.kt
@@ -0,0 +1,53 @@
+package com.appchef.dishapp
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.appchef.dishapp.databinding.ActivitySplashScreenBinding
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val splashScreenBinding: ActivitySplashScreenBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(splashScreenBinding.root)
+
+        // To make splash screen Full Screen
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val splashAnimation = AnimationUtils.loadAnimation(this,R.anim.anim_splash)
+        splashScreenBinding.tvSplashHeading.animation = splashAnimation
+
+        splashAnimation.setAnimationListener(object : Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+              // ......
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+              Handler(Looper.getMainLooper()).postDelayed({
+                  startActivity(Intent(this@SplashScreen,MainActivity::class.java))
+                  finish()
+              },700)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+               // ......
+            }
+        })
+    }
+}
