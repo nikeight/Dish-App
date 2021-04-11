9d0403e6794dae930c7be1208286038e506f1217
Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
2021-04-11 23:59:00 +0530

    Glide Lib added

--- app/src/main/java/com/appchef/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/appchef/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -19,6 +19,7 @@ import android.widget.Toast
 import com.appchef.dishapp.R
 import com.appchef.dishapp.databinding.ActivityAddUpdateDishBinding
 import com.appchef.dishapp.databinding.CustomDialogImageSelectionBinding
+import com.bumptech.glide.Glide
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.DexterBuilder
 import com.karumi.dexter.MultiplePermissionsReport
@@ -35,7 +36,7 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mBinding: ActivityAddUpdateDishBinding
 
-    companion object{
+    companion object {
         private const val CAMERA = 1
         private const val GALLERY = 2
     }
@@ -101,12 +102,12 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
                 Manifest.permission.CAMERA
             ).withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                   report?.let {
-                       if (report!!.areAllPermissionsGranted()) {
-                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                           startActivityForResult(intent, CAMERA)
-                       }
-                   }
+                    report?.let {
+                        if (report!!.areAllPermissionsGranted()) {
+                            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            startActivityForResult(intent, CAMERA)
+                        }
+                    }
                 }
 
                 override fun onPermissionRationaleShouldBeShown(
@@ -128,9 +129,11 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
                 Manifest.permission.READ_EXTERNAL_STORAGE,
             ).withListener(object : PermissionListener {
                 override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
-                    val galleryIntent = Intent(Intent.ACTION_PICK,
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                        startActivityForResult(galleryIntent, GALLERY)
+                    val galleryIntent = Intent(
+                        Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                    )
+                    startActivityForResult(galleryIntent, GALLERY)
                 }
 
                 override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
@@ -173,26 +176,32 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK){
-            if (requestCode ==  CAMERA){
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA) {
                 // Setting the bitmap to the IV
                 data?.let {
-                    val thumbnail : Bitmap = data.extras!!.get("data") as Bitmap
+                    val thumbnail: Bitmap = data.extras!!.get("data") as Bitmap
                     mBinding.ivDishImage.setImageBitmap(thumbnail)
                 }
             }
 
-            if (requestCode == GALLERY){
+            if (requestCode == GALLERY) {
                 // Setting the URI to the ImageView
                 data?.let {
 
                     val selectedPhotoUri = data.data
 
-                    mBinding.ivDishImage.setImageURI(selectedPhotoUri)
+                    // Loading Image by using Glide.
+                    Glide.with(this)
+                        .load(selectedPhotoUri)
+                        .centerCrop()
+                        .into(mBinding.ivDishImage)
+
+//                    mBinding.ivDishImage.setImageURI(selectedPhotoUri)
                 }
             }
-        }else if (requestCode == Activity.RESULT_CANCELED){
-            Log.e("Cancelled","User Cancelled image selection")
+        } else if (requestCode == Activity.RESULT_CANCELED) {
+            Log.e("Cancelled", "User Cancelled image selection")
         }
     }
 }
