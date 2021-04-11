08d1bc272fe49e9438197ef7a230d6559791837e
Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
2021-04-11 16:02:55 +0530

    Binary XML error solved

--- app/src/main/java/com/appchef/dishapp/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/appchef/dishapp/view/activities/AddUpdateDishActivity.kt
@@ -7,46 +7,56 @@ import android.widget.Toast
 import com.appchef.dishapp.R
 import com.appchef.dishapp.databinding.ActivityAddUpdateDishBinding
 
-class AddUpdateDishActivity : AppCompatActivity(R.layout.activity_add_update_dish), View.OnClickListener {
-    // Adding and updating the object here.
+class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
+    // END
 
-    private val mBinding: ActivityAddUpdateDishBinding = TODO()
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setupActionBar()
 
-        // TODO Step 5: Assign the click event to the image button.
-        // START
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupActionBar()
+//
+//        // TODO Step 5: Assign the click event to the image button.
+//        // START
         mBinding.ivAddDishImage.setOnClickListener(this@AddUpdateDishActivity)
-        // END
+//        // END
     }
 
+    // TODO Step 4: Override the onclick listener method.
+    // START
+    override fun onClick(v: View) {
+
+        // TODO Step 6: Perform the action when user clicks on the addDishImage and show Toast message for now.
+        // START
+        when (v.id) {
+
+            R.id.iv_add_dish_image -> {
+
+                Toast.makeText(this@AddUpdateDishActivity, "You have clicked on the ImageView.", Toast.LENGTH_SHORT).show()
+
+                return
+            }
+        }
+        // END
+    }
+    // END
+
+    /**
+     * A function for ActionBar setup.
+     */
     private fun setupActionBar() {
         setSupportActionBar(mBinding.toolbarAddDishActivity)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//        // TODO Step 2: Replace the back button that we have generated.
-//        // START
-//        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
-//        // END
+        // TODO Step 2: Replace the back button that we have generated.
+        // START
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+        // END
 
         mBinding.toolbarAddDishActivity.setNavigationOnClickListener { onBackPressed() }
-    }
-
-    override fun onClick(v: View?) {
-        if (v != null) {
-            when (v.id) {
-                R.id.iv_add_dish_image -> {
-                    Toast.makeText(
-                        this@AddUpdateDishActivity,
-                        "You have clicked on the ImageView.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-
-                    return
-                }
-            }
-        }
     }
 }
